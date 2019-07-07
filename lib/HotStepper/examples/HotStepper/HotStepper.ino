d6d9e4af3ef4b9ee0ff26c682d66603b7bb8c6ca
Rob Carey <rob_carey_1@hotmail.com>
2019-07-07 11:58:37 +1000

    Inital commit with stepper libs

---
+++ lib/HotStepper/examples/HotStepper/HotStepper.ino
@@ -0,0 +1,32 @@
+// Include the library
+#include <HotStepper.h>
+
+// Instatiate some stepper motors
+HotStepper stepper1(&PORTB, 0b00011101);
+HotStepper stepper2(&PORTD, 0b11110000);
+
+byte dir1;
+byte dir2;
+
+void setup(){
+  dir1 = FORWARD;
+  dir2 = FORWARD;
+  // Make sure the ports and timer are configured
+  HotStepper::setup();
+  // Control our steppers
+  stepper1.turn(10, dir1);
+  stepper2.turn(20, dir2);
+}
+
+
+void loop(){
+  // ready will return true when the stepper has moved the desired number of steps
+  if(stepper1.ready()){
+    dir1 = !dir1;
+    stepper1.turn(1000, dir1);
+  }
+  if(stepper2.ready()){
+    dir2 = !dir2;
+    stepper2.turn(2000, dir2);
+  }
+}
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include "Arduino.h"
+#include "BasicStepperDriver.h"
+#include "DRV8834.h"
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
